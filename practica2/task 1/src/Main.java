7f58c3e8f8c78ced1801d54866445bd498f7362b
Lorddeshvut <87749418+Lorddeshvut@users.noreply.github.com>
2022-05-17 15:29:47 +0300

    Update Main.java

--- practica2/task 1/src/Main.java
+++ practica2/task 1/src/Main.java
@@ -1,3 +1,5 @@
+//Определить одномерный массив и заполнить его случайными значениями. Дан массив c(n). Переписать в массив x(n) все ненулевые элементы умноженные на 4.
+
 package com.company;
 
 public class Main {
@@ -8,43 +10,21 @@ public class Main {
             System.out.println(array[i]);
         }
     }
-    private static int[] sortVector(int[] vector)
-    {
-        boolean flag;
-        int temp;
-        do {
-            flag = false;
-            for (int i = 0; i < vector.length-1; i++) {
-                if (vector[i] > vector[i+1])
-                {
-                    temp = vector[i];
-                    vector[i] = vector[i+1];
-                    vector[i+1] = temp;
-                    flag = true;
-                }
-            }
-        } while (flag);
-        return vector;
-    }
     public static void main(String[] args) {
 
+        int n=10;
 
-        int [] x={3,4,5,2,99,56,34,6,1,457,86,356,56,35,98,54,6,5,3,76,54,5};
-        for(int i=0;i<x.length;i++){
-            if (x[i]==1){
-                int [] t=new int[x.length-i-1];
-                int i1=i;
-                for(int j=0;i1<x.length-1;i1++,j++){
-                    t[j]=x[i1+1];
-
-                }
-                t=sortVector(t);
-                i1=i;
-                for(int j=i1+1,k=0;j<x.length;j++,k++){
-                    x[j]=t[k];
-                }
-                printArray(x);
+        int [] c=new int[n];
+        int [] x=new int[n];
+        for (int i=0, j=0;i<c.length;i++) {
+            c[i]= (int) (50-Math.random()*100);
+            if (c[i]!=0){
+                x[j]=c[i]*4;
+                j++;
             }
         }
+
+        printArray(c);
+        printArray(x);
     }
 }
