26515a64f73ae8ef02fe4852db125d5f1862c862
Lorddeshvut <87749418+Lorddeshvut@users.noreply.github.com>
2022-05-26 14:56:06 +0300

    Update TestRoom.java

--- re/src/re/TestRoom.java
+++ re/src/re/TestRoom.java
@@ -1,8 +1,14 @@
+// Вариант 2 Система Управления доставкой товара.
+// Id - Идентификатор
+//name - название товара
+//courier - курьер
+//dateTime - Дата и время (String);
+//type - тип заказа
 package re;
 
 public class TestRoom {
 	public static void main(String[] args){
-		Room room=new Room(116,12,50,5,5200);
+		Room room=new Room(116,ванна,иван,5,1);
 		System.out.println(room.toString());
 
 		}
